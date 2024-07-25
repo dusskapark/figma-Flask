58be66145d2026b2f4659290234203411c40e6fe
JooHyung Park <dusskapark@gmail.com>
2024-07-26 02:07:44 +0800

    Update controller.ts

--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,5 +1,33 @@
 import { PluginMessage } from '../typings/types';
+import { marked } from 'marked';
 
+let reportPollingInterval: number | null = null;
+
+function startPollReport() {
+  if (reportPollingInterval === null) {
+    reportPollingInterval = setInterval(async () => {
+      try {
+        const response = await fetch('http://localhost:5000/get_report');
+        const data = await response.json();
+        if (data.status === 'success') {
+          // parse markdown content
+          const html = marked(data.content);
+          console.log('Report update received:', html);
+
+        }
+      } catch (error) {
+        console.error('Error polling report:', error);
+      }
+    }, 30000);
+  }
+}
+
+function stopPollReport() {
+  if (reportPollingInterval !== null) {
+    clearInterval(reportPollingInterval);
+    reportPollingInterval = null;
+  }
+}
 figma.showUI(__html__, { width: 400, height: 500 });
 
 figma.ui.onmessage = async (msg: PluginMessage) => {
@@ -20,10 +48,12 @@ figma.ui.onmessage = async (msg: PluginMessage) => {
     });
     const data = await response.json();
     figma.ui.postMessage({ type: 'response', data });
+    startPollReport();
   } else if (msg.type === 'stop-exploration') {
     const response = await fetch('http://localhost:5000/stop_exploration', { method: 'POST' });
     const data = await response.json();
     figma.ui.postMessage({ type: 'response', data });
+    stopPollReport();
   } else if (msg.type === 'exploration-status') {
     const response = await fetch('http://localhost:5000/exploration_status');
     const data = await response.json();
